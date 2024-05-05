cebd3701ab116da45bbe9297898b90cc7a4c0530
Najma099 <najmakhatun0999@gmail.com>
2024-05-05 12:57:55 +0530

    HTML lec 2

---
+++ HTML-Practice/javascript/Helloworld.js
@@ -0,0 +1,9 @@
+console.log('Hello World');
+let name='najma';
+console.log(name);
+let x=3;
+console.log(x);
+// variable name should be meaningful
+// cannot be reserved keyword
+//cannot contain space
+let firstName
---
+++ HTML-Practice/javascript/class_3.js
@@ -0,0 +1,24 @@
+// const pi=Math.PI;
+// console.log(pi);
+// console.log(Math.round(pi));
+
+// let num=5.2;
+// console.log(Math.ceil(num));
+// console.log(Math.floor(num));
+// console.log(Math.random());
+// console.log( Math.round(Math.random()*11));
+
+//STRING
+// let a=2;
+// let b=3;
+// console.log(`the sum of ${a} and ${b} is ${a+b}`);
+
+//ARRAY
+//  let arr1=[1,2,3,4,5,6,7,8,9,10]
+//  arr1[3]=14;
+// //let arr2=Array();// array constructor
+// let arr2="Java script";
+// console.log(arr1[4]);
+
+// console.log(arr2.split(" "));
+
