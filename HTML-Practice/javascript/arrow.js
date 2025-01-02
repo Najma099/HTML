58be29e68326352aa964f4ddd232a43527c0601b
najma099 <najmakhatun0999@gmail.com>
2025-01-02 07:14:52 +0530

    arrow function

---
+++ HTML-Practice/javascript/arrow.js
@@ -0,0 +1,21 @@
+// normal function 
+
+console.log(one(7));
+function one(num) {
+    return num + 1;
+};
+
+
+const arr = function (numm ) {
+    return numm + 2;
+};
+
+// we need to instialise the variable first
+console.log(arr(5));
+
+
+const brr = (numm) => {
+    return (numm * numm)
+};
+console.log(brr(7));
+
