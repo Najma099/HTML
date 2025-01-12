b0d83701b9d5e35b5ed10d0261869b7538eb4538
najma099 <najmakhatun0999@gmail.com>
2025-01-12 13:05:33 +0530

    Change Change

---
+++ HTML-Practice/Project/Project_05/script.js
@@ -0,0 +1,24 @@
+
+let intervalid;
+function change() {
+    const hex = '0123456789ABCDEF';
+    let color = '#';
+    for(let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+const changecolor = function () {
+    intervalid = setInterval(() => {
+         document.body.style.backgroundColor = change();
+    },1000)
+ }
+ 
+document.querySelector('#start').addEventListener('click', changecolor)
+document.querySelector('#stop').addEventListener('click', stopcolour);
+
+
+
+function stopcolour() {
+    clearInterval(intervalid);
+}
