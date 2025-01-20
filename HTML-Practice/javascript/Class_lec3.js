2ab33be0dd160f96d7b863702f5d37b473034ac0
najma099 <najmakhatun0999@gmail.com>
2025-01-20 16:20:43 +0530

    Calculator

---
+++ HTML-Practice/javascript/Class_lec3.js
@@ -0,0 +1,51 @@
+// const add = function(a , b) {
+//     return a + b;
+// }
+// console.log(add(3,4));
+
+
+// const plant = {
+
+//     name: "Najma",
+//     greet: function() {
+//         console.log(this.name);
+//     }
+
+// };
+
+// plant.greet();
+
+
+// const person = [
+//     function() {
+//         console.log("First function!");
+//     },
+//     function() {
+//         console.log("Second Function");
+//     },
+//     function() {
+//         console.log("third Function");
+//     }
+// ]
+
+// for(const key of person) {
+//     key();
+// }
+
+
+function hii() {
+    namee: "Najma";
+    console.log("hiiiiii");
+}
+
+function fun() {
+    return hii();
+}
+//fun();
+
+
+function funn(hii) {
+    console.log(hii.namee);
+}
+
+funn();
