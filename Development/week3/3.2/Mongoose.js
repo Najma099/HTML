77afd331f0e62e6952a690aabd0faa585ce830a9
najma099 <najmakhatun0999@gmail.com>
2025-04-10 18:31:43 +0530

    First data Entry to backend🎉

---
+++ Development/week3/3.2/Mongoose.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const User = mongoose.model("User", {
+  name: String,
+  email: String,
+  password: String,
+});
+
+mongoose.connect(
+  "mongodb+srv://Najma:Ru2pa4mo6da8@najma.pq370co.mongodb.net/newdb",
+);
+
+app.use(express.json());
+app.post("/signin", async (req, res) => {
+  const name = req.body.name;
+  const password = req.body.password;
+  const email = req.body.email;
+
+  const existing = await User.findOne({ name, email });
+  if (existing) return res.status(401).send("Username already exist");
+  const newuser = new User({
+    name,
+    email,
+    password,
+  });
+  await newuser.save();
+  res.status(200).send("User register SuccessFull");
+});
+
+app.listen(3000);
