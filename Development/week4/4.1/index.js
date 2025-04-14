c1053c57f14784ca862531443abdb8ba05af18b3
najma099 <najmakhatun0999@gmail.com>
2025-04-14 12:08:00 +0530

    Server added

--- Development/week4/4.1/index.js
+++ Development/week4/4.1/index.js
@@ -2,3 +2,16 @@ import express from 'express';
 const app = express();
 
 app.use(express.json());
+app.use('/sum', (req, res) => {
+  const a = parseInt(req.query.a);
+  const b = parseInt(req.query.b);
+  
+  if (isNaN(a) || isNaN(b)) {
+    return res.status(400).send("Invalid input: both 'a' and 'b' must be numbers");
+  }
+  res.send(`${a + b}`);
+})
+
+app.listen(3000,() => {
+  console.log('Server is running at port 3000');
+})
