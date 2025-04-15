cedb50d8a18e9f63baee3dc065781de675b51c71
najma099 <najmakhatun0999@gmail.com>
2025-04-15 12:51:42 +0530

    Counter-App react

---
+++ Development/week5/counter-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <CustonBtn count = {count} setCount = {setCount} ></CustonBtn>
+  )
+}
+
+function CustonBtn(props) {
+  function onClickHandler() {
+    props.setCount(props.count + 1);
+  }
+  return <button onClick = {onClickHandler}>Counter { props.count}</button>
+}
+export default App
