b3eca8fb528ea2e680a43057f48b1cf86384e072
najma099 <najmakhatun0999@gmail.com>
2025-04-23 20:36:28 +0530

    User Zod Schema

---
+++ assignments/week-8/Backend/src/ZodSchema/user.schema.js
@@ -0,0 +1,29 @@
+import { z } from "zod";
+
+export const UserSchema = z.object({
+  username: z.string().email('Invalid email format'),
+  password: z.string().min(8, 'Minimum 8 characters'),
+  firstName: z.string(),
+  lastName: z.string()
+});
+
+export const SignInSchema = UserSchema.pick({
+  username: true,
+  password: true
+});
+
+export const UpdateSchema = z.object({
+  username: z.string().optional(),
+  firstName: z.string().optional(),
+  lastName: z.string().optional()
+});
+
+export const PasswordSchema = z.object({
+  oldPassword: z.string().min(8, 'Minimum 8 characters'),
+  newPassword: z.string().min(8, 'Minimum 8 characters'),
+  confirmPassword: z.string().min(8, 'Minimum 8 characters')
+})
+  .refine((data) => data.newPassword === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ['confirmPassword'],
+  });
