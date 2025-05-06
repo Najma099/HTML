1948290dd346519065a696e57061a3b5af3b1ef6
najma099 <najmakhatun0999@gmail.com>
2025-05-06 09:43:59 +0530

    Prisma

--- Development/week_10/dist/index.js
+++ Development/week_10/dist/index.js
@@ -9,38 +9,55 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const pg_1 = require("pg");
-const client = new pg_1.Client({
-    connectionString: "postgresql://user_owner:npg_AcsI4xUu1mat@ep-quiet-dream-a4edhghm-pooler.us-east-1.aws.neon.tech/user?sslmode=require"
-});
-function createUserTable() {
+const prisma_1 = require("./generated/prisma");
+const prisma = new prisma_1.PrismaClient();
+function createUser() {
     return __awaiter(this, void 0, void 0, function* () {
-        try {
-            yield client.connect();
-            // Create table if it doesn't exist
-            yield client.query(`
-      CREATE TABLE IF NOT EXISTS users (
-          id SERIAL PRIMARY KEY,
-          username VARCHAR(50) UNIQUE NOT NULL,
-          email VARCHAR(255) UNIQUE NOT NULL,
-          password VARCHAR(255) NOT NULL,
-          created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-      );
-    `);
-            console.log("Table ensured.");
-            // Insert user data (example)
-            const username = 'najma';
-            const email = 'najma@example.com';
-            const password = 'secure123';
-            const insertResult = yield client.query(`INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *;`, [username, email, password]);
-            console.log("Inserted user:", insertResult.rows[0]);
-        }
-        catch (err) {
-            console.error("Error:", err);
-        }
-        finally {
-            yield client.end();
-        }
+        const user = yield prisma.user.create({
+            data: {
+                email: "najmakhatun099@gmail",
+                firstName: "najma",
+                lastName: "khatun",
+                password: "765abcd"
+            },
+            select: {
+                email: true,
+                firstName: true
+            }
+        });
     });
 }
-createUserTable();
+createUser();
+// import { Client } from 'pg';
+// const client = new Client({
+//   connectionString: "postgresql://neondb_owner:npg_gLMEsJt23KnA@ep-frosty-sky-a4f7cg7b-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require"
+// });
+// client.connect();
+// async function createUserTable() {
+//   try {
+//     await client.query(`
+//       CREATE TABLE IF NOT EXISTS users (
+//           id SERIAL PRIMARY KEY,
+//           username VARCHAR(50) UNIQUE NOT NULL,
+//           email VARCHAR(255) UNIQUE NOT NULL,
+//           password VARCHAR(255) NOT NULL,
+//           created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+//       );
+//     `);
+//     console.log("Table ensured.");
+//     // Insert user data (example)
+//     const username = 'najma';
+//     const email = 'najma@example.com';
+//     const password = 'secure123';
+//     const insertResult = await client.query(
+//       `INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *;`,
+//       [username, email, password]
+//     );
+//     console.log("Inserted user:", insertResult.rows[0]);
+//   } catch (err) {
+//     console.error("Error:", err);
+//   } finally {
+//     await client.end();
+//   }
+// }
+// createUserTable();
--- Development/week_10/src/index.ts
+++ Development/week_10/src/index.ts
@@ -1,40 +1,70 @@
-import { Client } from 'pg';
+import { PrismaClient } from "./generated/prisma";
+const prisma = new PrismaClient();
 
-const client = new Client({
-  connectionString: "postgresql://user_owner:npg_AcsI4xUu1mat@ep-quiet-dream-a4edhghm-pooler.us-east-1.aws.neon.tech/user?sslmode=require"
-});
-
-async function createUserTable() {
-  try {
-    await client.connect();
-
-    await client.query(`
-      CREATE TABLE IF NOT EXISTS users (
-          id SERIAL PRIMARY KEY,
-          username VARCHAR(50) UNIQUE NOT NULL,
-          email VARCHAR(255) UNIQUE NOT NULL,
-          password VARCHAR(255) NOT NULL,
-          created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-      );
-    `);
-    console.log("Table ensured.");
-
-    // Insert user data (example)
-    const username = 'najma';
-    const email = 'najma@example.com';
-    const password = 'secure123';
-
-    const insertResult = await client.query(
-      `INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *;`,
-      [username, email, password]
-    );
-
-    console.log("Inserted user:", insertResult.rows[0]);
-  } catch (err) {
-    console.error("Error:", err);
-  } finally {
-    await client.end();
-  }
+async function createUser() {
+  const user = await prisma.user.create({
+    data: {
+      email: "najmakhatun099@gmail",
+      firstName: "najma",
+      lastName: "khatun",
+      password: "765abcd"
+    },
+    select: {
+      email: true,
+      firstName: true
+    }
+  });
 }
+createUser()
 
-createUserTable();
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Client } from 'pg';
+
+// const client = new Client({
+//   connectionString: "postgresql://neondb_owner:npg_gLMEsJt23KnA@ep-frosty-sky-a4f7cg7b-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require"
+// });
+// client.connect();
+
+// async function createUserTable() {
+//   try {
+//     await client.query(`
+//       CREATE TABLE IF NOT EXISTS users (
+//           id SERIAL PRIMARY KEY,
+//           username VARCHAR(50) UNIQUE NOT NULL,
+//           email VARCHAR(255) UNIQUE NOT NULL,
+//           password VARCHAR(255) NOT NULL,
+//           created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+//       );
+//     `);
+//     console.log("Table ensured.");
+
+//     // Insert user data (example)
+//     const username = 'najma';
+//     const email = 'najma@example.com';
+//     const password = 'secure123';
+
+//     const insertResult = await client.query(
+//       `INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *;`,
+//       [username, email, password]
+//     );
+
+//     console.log("Inserted user:", insertResult.rows[0]);
+//   } catch (err) {
+//     console.error("Error:", err);
+//   } finally {
+//     await client.end();
+//   }
+// }
+
+// createUserTable();
