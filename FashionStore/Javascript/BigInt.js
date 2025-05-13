e2ac84fed47fcd85e84bc72d9aa656ec3b544335
najma099 <najmakhatun0999@gmail.com>
2025-05-13 20:55:16 +0530

    different methods of string - charCodeAt charAt indexOf endsWith startWith slice substring

---
+++ FashionStore/Javascript/BigInt.js
@@ -0,0 +1,11 @@
+let a = Number.MAX_SAFE_INTEGER;
+let b = a + 2;
+let c = a + 1;
+console.log(a);
+console.log(b);
+console.log(c);
+
+//console.log((alert("Enter any number")));
+let aa = parseInt("TEN");
+let bb = parseINT("100");
+let cc = Number("34");
---
+++ FashionStore/Javascript_String/slice.js
@@ -0,0 +1,34 @@
+let str = "sachinetendulkarcricket";
+console.log(str.slice(6, -5));
+console.log(str.slice(6, 16));
+colsole.log(str.slice());
+colsole.log(str.slice(-6, -9));
+colsole.log(str.slice(-9, -6));
+colsole.log(str.slice(NaN, NaN));
+colsole.log(str.slice(undefined, undefined));
+colsole.log(str.slice(null, null));
+colsole.log(str.slice(null,undefined));
+colsole.log(str.slice(true, true));
+colsole.log(str.slice(true, false));
+colsole.log(str.slice(false, true));
+colsole.log(str.slice(false, null));
+colsole.log(str.slice(false, undefined));
+
+
+
+console.log(str.substring(6, -5));
+console.log(str.substring(6, 16));
+colsole.log(str.substring());
+colsole.log(str.substring(-6, -9));
+colsole.log(str.substring(-9, -6));
+colsole.log(str.substring(NaN, NaN));
+colsole.log(str.substring(undefined, undefined));
+colsole.log(str.substring(null, null));
+colsole.log(str.substring(null,undefined));
+colsole.log(str.substring(true, true));
+colsole.log(str.substring(true, false));
+colsole.log(str.substring(false, true));
+colsole.log(str.substring(false, null));
+colsole.log(str.substring(false, undefined));
+
+
