d3e5b4ab3e72232cae7259113cd49e3a9a5f71bc
najma099 <najmakhatun0999@gmail.com>
2025-05-19 12:03:26 +0530

    changed api domain typo in frontend

--- assignments/week-8/Frontend/src/store/balance.atom.js
+++ assignments/week-8/Frontend/src/store/balance.atom.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 import { atom, selector } from "recoil";
+import { apiDomain } from "../utils/config";
+
 
 export const balanceAtom = atom({
   key: "balanceAtom",
   default: selector({
     key: "balanceSelector",
     get: async () => {
-      const response = await axios.get('http://localhost:5001/api/v1/account/balance',
+      const response = await axios.get(apiDomain + '/api/v1/account/balance',
         {
           withCredentials: true,
         });
