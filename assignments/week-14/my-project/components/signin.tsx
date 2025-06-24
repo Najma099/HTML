9dde39bbbf9874cc45e54f255b266215929511ab
najma099 <najmakhatun0999@gmail.com>
2025-06-24 23:58:59 +0530

    Next js Basic app

--- assignments/week-14/my-project/components/signin.tsx
+++ assignments/week-14/my-project/components/signin.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+"use client"
+
 interface LabelledInputType {
   label: string;
   placeholder: string;
@@ -19,10 +20,11 @@ function LabelledInput({ label, placeholder, type }: LabelledInputType) {
     </div>
   );
 }
-
-export default function Signin() {
-  return (
-    <div className="h-screen flex justify-center flex-col">
+function handler() {
+    console.log("Hiii");
+}
+export default function SigninComponent() {
+    return    <div className="h-screen flex justify-center flex-col">
       <div className="flex justify-center">
         <a
           href="#"
@@ -38,6 +40,7 @@ export default function Signin() {
               <button
                 type="button"
                 className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+                onClick={handler}
               >
                 Sign in
               </button>
@@ -46,5 +49,4 @@ export default function Signin() {
         </a>
       </div>
     </div>
-  );
 }
