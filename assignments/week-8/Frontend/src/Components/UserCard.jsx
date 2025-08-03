4536619aeb27129d426497d2939558bbaa6136ae
najma099 <najmakhatun0999@gmail.com>
2025-08-03 13:22:47 +0530

    .

--- assignments/week-8/Frontend/src/Components/UserCard.jsx
+++ assignments/week-8/Frontend/src/Components/UserCard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React,{ useState } from "react";
 import axios from "axios";
 import {Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
 import { useSetRecoilState } from "recoil";
